71e2e3d66448d53f083e47192fba0b65b4c83be5
Alexander Santos <39683032+EminentCoding@users.noreply.github.com>
2019-06-01 18:01:33 -0400

    Finished Exercise 8

--- ex8/script.js
+++ ex8/script.js
@@ -5,3 +5,50 @@
     BONUS) Add a functioning "delete" button to each task (HINT: use the keyword "this" in your delete button's click listener! Console log "this" to see what value it holds)
     SUPER BONUS) Make each task editable
 */
+
+var newTask = document.getElementById("description");
+var submitBtn = document.getElementById("add-button");
+var finishBtns = document.getElementsByClassName("finishbtn")
+
+
+submitBtn.addEventListener('click', function() {
+
+    //Declae all the variables that we will use in this function
+    var taskItem = document.createElement("li");
+
+    var finishbtn = document.createElement('button');
+    //Set all the attributes&Classes to our new elements
+    taskItem.setAttribute("onclick", "editItems()");
+    finishbtn.setAttribute('class', "finishbtns")
+    finishbtn.innerText = "Finish";
+    //Append all the elements we created to the parent.
+
+    taskItem.innerText = document.getElementById("description").value;
+    document.getElementById("todo-list").appendChild(taskItem);
+
+    finishbtn.addEventListener('click', function() {
+        if (finishbtn.parentElement.classList.contains('finished')) {
+            finishbtn.parentElement.classList.remove('finished')
+        } else {
+            finishbtn.parentElement.classList.add('finished')
+        }
+    })
+})
+
+
+
+
+
+
+
+var editItems = function() {
+    var taskList = document.querySelectorAll('li');
+    //Loop through the array of li elements
+    for (let i = 0; i < taskList.length; i++) {
+        const element = taskList[i];
+        element.addEventListener('click', function() {
+            document.getElementById('description').value = this.innerText;
+            element.parentNode.removeChild(element)
+        })
+    }
+}
